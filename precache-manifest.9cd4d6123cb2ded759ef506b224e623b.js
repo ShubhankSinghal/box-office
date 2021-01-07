d5f3991e0ae954dd3d9c973b057dfd2aec5932c5
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-01-07 20:28:13 +0530

    Updates

---
+++ precache-manifest.9cd4d6123cb2ded759ef506b224e623b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfba3ed511c7c4dc8f0f7f3704c9b35",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "ad78eb504f195a962e87",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "4e572692a5fc268f9dbf",
+    "url": "/box-office/static/js/2.5bbfd9d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.5bbfd9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad78eb504f195a962e87",
+    "url": "/box-office/static/js/main.0bfc9eef.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
